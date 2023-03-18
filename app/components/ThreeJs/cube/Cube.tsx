73110c4ab3aa4848498ef530a332f0087614eb29
Jacob Mutale <mutale1808@gmail.com>
2023-03-18 14:35:32 +0200

    change the color of the cube

--- app/components/ThreeJs/cube/Cube.tsx
+++ app/components/ThreeJs/cube/Cube.tsx
@@ -4,18 +4,21 @@ import React, { useRef } from 'react'
 
 const Cube = () => {
     const textRef = useRef()
-    useFrame(state => (textRef.current.position.x = Math.sin(state.clock.elapsedTime)*2))
+    // TODO: YOU CAN CHANGE THE SPEED OF THE TEXT HERE
+    useFrame(state => (textRef.current.position.x = Math.sin(state.clock.elapsedTime) * 0.5))
     return (
         <mesh>
             <boxGeometry args={[2, 2, 2]} />
             <meshStandardMaterial >
                 <RenderTexture attach="map">
+                    {/* TODO:YOU CAN CHANGE THE PERSPECTIVE OF THE TEXT HERE */}
                     <PerspectiveCamera
                         makeDefault
                         position={[0, 0, 2]}
                     />
-                    <color attach="background" args={["black"]} />
-                    <Text ref={textRef} fontSize={1} color="white">
+                    {/* TODO:YOU CAN CHANGE THE COLOR AND TEXT HERE OF THE CUBE*/}
+                    <color attach="background" args={["blue"]} />
+                    <Text ref={textRef} fontSize={0.5} color="white">
                         hello
                     </Text>
                 </RenderTexture>
